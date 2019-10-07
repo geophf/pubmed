5032bafd808adbac2b00b8817503c33e72b17f9c
geophf <dauclair@hotmail.com>
2019-10-07 16:34:51 -0400

    Added processing information to data_load table

--- data_model/scripts/pubmed_postgres_create.sql
+++ data_model/scripts/pubmed_postgres_create.sql
@@ -1,15 +1,19 @@
 DROP TABLE IF EXISTS "data_load";
+DROP INDEX IF EXISTS "data_load_downloaded_ind_idx";
 
 CREATE TABLE "data_load" (
 	"id" serial NOT NULL,
 	"packet_name" TEXT NOT NULL,
 	"downloaded_ind" integer NOT NULL,
 	"downloaded_dttm" TIMESTAMP,
+	"processed_ind" integer,
+	"processing_error" TEXT,
 	CONSTRAINT "data_load_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
+CREATE INDEX data_load_downloaded_ind_idx ON data_load (downloaded_ind);
 
 -- LOOKUP TABLES -------------------------------------------------------------
 
@@ -23,5 +27,3 @@ CREATE TABLE "duality_lk" (
 
 INSERT INTO duality_lk (id,truth_serum)
 VALUES (1,'Y'), (2,'N');
-
-
